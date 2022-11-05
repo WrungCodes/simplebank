5e2d52ea90ef08602f797030518db9293a80974a
Daniel Fayemi <daniel.fayemi@latoken.com>
2022-11-05 21:30:14 +0100

    80% account test coverage

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,13 +2,14 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/WrungCodes/simplebank/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func CreateAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -25,4 +26,83 @@ func TestCreateAccount(t *testing.T) {
 
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	CreateAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account := CreateAccount(t)
+
+	retrieved, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, retrieved)
+
+	require.Equal(t, account.Balance, retrieved.Balance)
+	require.Equal(t, account.Owner, retrieved.Owner)
+	require.Equal(t, account.Currency, retrieved.Currency)
+	require.Equal(t, account.ID, retrieved.ID)
+	require.Equal(t, account.CreatedAt, retrieved.CreatedAt)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account := CreateAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      account.ID,
+		Balance: util.RandomMoney(),
+	}
+
+	updated, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, updated)
+
+	require.Equal(t, updated.Balance, arg.Balance)
+
+	require.Equal(t, account.Owner, updated.Owner)
+	require.Equal(t, account.Currency, updated.Currency)
+	require.Equal(t, account.ID, updated.ID)
+	require.Equal(t, account.CreatedAt, updated.CreatedAt)
+
+	retrieved, err := testQueries.GetAccount(context.Background(), account.ID)
+
+	require.Equal(t, updated.Balance, retrieved.Balance)
+	require.Equal(t, updated.Owner, retrieved.Owner)
+	require.Equal(t, updated.Currency, retrieved.Currency)
+	require.Equal(t, updated.ID, retrieved.ID)
+	require.Equal(t, updated.CreatedAt, retrieved.CreatedAt)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account := CreateAccount(t)
+
+	err := testQueries.DeleteAccount(context.Background(), account.ID)
+	require.NoError(t, err)
+
+	deleted, err := testQueries.GetAccount(context.Background(), account.ID)
+	require.Error(t, err)
+	require.Empty(t, deleted)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+}
+
+func TestListAcconts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		CreateAccount(t)
+	}
+
+	arg := ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
 }
